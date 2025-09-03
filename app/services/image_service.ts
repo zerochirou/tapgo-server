5d13e126d31ce5ace2e369123c4c97d321011cc0
zerochirou <-force>
2025-09-03 21:19:58 +0700

    refactor(supabase_bucket): menempatkan nama supabase bucket di env

--- app/services/image_service.ts
+++ app/services/image_service.ts
@@ -3,20 +3,22 @@ import { v4 as uuid } from 'uuid'
 import fs from 'node:fs'
 
 export default class ImageService {
+  constructor(protected bucket: string = process.env.SUPABASE_BUCKET as string) {}
+
   public async uploadBackgroundImage(file: any): Promise<any> {
     if (!file) throw new Error('No file uploaded')
 
     const fileName = `background_images/background-image-${uuid()}-${file.clientName}`
     const fileBuffer = fs.readFileSync(file.tmpPath!)
 
-    const { error } = await supabase.storage.from('images_pool').upload(fileName, fileBuffer, {
+    const { error } = await supabase.storage.from(this.bucket).upload(fileName, fileBuffer, {
       contentType: file.type,
       upsert: true,
     })
 
     if (error) throw error
 
-    const { data: publicUrl } = supabase.storage.from('images_pool').getPublicUrl(fileName)
+    const { data: publicUrl } = supabase.storage.from(this.bucket).getPublicUrl(fileName)
     const imgUrl = publicUrl.publicUrl
     return { backgroundImageUrl: imgUrl, backgroundImageFileName: fileName }
   }
@@ -27,14 +29,14 @@ export default class ImageService {
     const fileName = `business_icon/business-icon-${uuid()}-${file.clientName}`
     const fileBuffer = fs.readFileSync(file.tmpPath!)
 
-    const { error } = await supabase.storage.from('images_pool').upload(fileName, fileBuffer, {
+    const { error } = await supabase.storage.from(this.bucket).upload(fileName, fileBuffer, {
       contentType: file.type,
       upsert: true,
     })
 
     if (error) throw error
 
-    const { data: publicUrl } = supabase.storage.from('images_pool').getPublicUrl(fileName)
+    const { data: publicUrl } = supabase.storage.from(this.bucket).getPublicUrl(fileName)
     const imageUrl = publicUrl.publicUrl
     return { businessIconUrl: imageUrl, businessIconFileName: fileName }
   }
@@ -45,20 +47,20 @@ export default class ImageService {
     const fileName = `menu_images/menu-images-${uuid()}-${file.clientName}`
     const fileBuffer = fs.readFileSync(file.tmpPath!)
 
-    const { error } = await supabase.storage.from('images_pool').upload(fileName, fileBuffer, {
+    const { error } = await supabase.storage.from(this.bucket).upload(fileName, fileBuffer, {
       contentType: file.type,
       upsert: true,
     })
 
     if (error) throw error
 
-    const { data: publicUrl } = supabase.storage.from('images_pool').getPublicUrl(fileName)
+    const { data: publicUrl } = supabase.storage.from(this.bucket).getPublicUrl(fileName)
     const imageUrl = publicUrl.publicUrl
     return { menuImageUrl: imageUrl, menuImageFileName: fileName }
   }
 
   public async removeImage(pathRelative: string) {
-    const { data, error } = await supabase.storage.from('images_pool').remove([pathRelative])
+    const { data, error } = await supabase.storage.from(this.bucket).remove([pathRelative])
 
     if (error) {
       throw new Error(`Gagal hapus file: ${error.message}`)
